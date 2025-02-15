3f0d8a5df2ed624acf48a7fc7e1e14e6017442da
SarthakCentocode <sarthak@centocode.com>
2025-02-15 13:27:31 +0530

    Increase request payload size limit for JSON and URL-encoded data

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/media", mediaRoutes);
